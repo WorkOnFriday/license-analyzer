be68978ae76f222469734367070959d6732b47fe
于斯同 <yusitong1999@foxmail.com>
2021-12-15 17:05:00 +0800

    重构: 本地模块的外部依赖分析,java import扫描,外部包查找和本地包查找

--- scanner/scanner.go
+++ scanner/scanner.go
@@ -4,7 +4,6 @@ import (
 	"archive/zip"
 	"bufio"
 	"encoding/xml"
-	"fmt"
 	"github.com/sirupsen/logrus"
 	"io"
 	"io/ioutil"
@@ -215,74 +214,89 @@ func deepScan(realFilePath, resultPath string, isLocal bool, local, external *[]
 	}
 }
 
-func shallowScan(path string) map[string]interface{} {
-	//If the dependent module only needs to scan the outermost layer, use this
-	result := make(map[string]interface{})
-	files, err := os.ReadDir(path)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	var tmpDir = path
-	if len(files) == 1 && files[0].IsDir() {
-		tmpDir = filepath.Join(path, files[0].Name())
-		files, err = os.ReadDir(tmpDir)
-	}
-	for _, file := range files {
-		name := strings.Split(strings.ToUpper(file.Name()), ".")
-		if name[0] == "LICENSE" || name[0] == "COPYING" || name[len(name)-1] == "LICENSE" {
-			//fileName, _ := filepath.Abs(file.Name())
-			fileName := filepath.Join(tmpDir, file.Name())
-			result[fileName] = scan(fileName)
-		}
-	}
-	return result
+////If the dependent module only needs to scan the outermost layer, use this
+//func shallowScan(path string) map[string]interface{} {
+//	result := make(map[string]interface{})
+//	files, err := os.ReadDir(path)
+//	if err != nil {
+//		log.Fatalln(err)
+//	}
+//	var tmpDir = path
+//	// 其中只有一个目录，则进入目录中搜索
+//	if len(files) == 1 && files[0].IsDir() {
+//		tmpDir = filepath.Join(path, files[0].Name())
+//		files, err = os.ReadDir(tmpDir)
+//	}
+//	// 扫描此层所有证书文件
+//	for _, file := range files {
+//		name := strings.Split(strings.ToUpper(file.Name()), ".")
+//		if name[0] == "LICENSE" || name[0] == "COPYING" || name[len(name)-1] == "LICENSE" {
+//			//fileName, _ := filepath.Abs(file.Name())
+//			fileName := filepath.Join(tmpDir, file.Name())
+//			result[fileName] = scan(fileName)
+//		}
+//	}
+//	return result
+//}
+
+type ModuleDependency struct {
+	Module     string
+	Dependency []string
 }
 
-func dependencyAnalyze(externalModules map[string][]string, local []map[string]interface{}) map[string][]string {
-	dependency := make(map[string][]string)
+type AllModuleDependency struct {
+	MainModule ModuleDependency
+	Modules    []ModuleDependency
+}
+
+func dependencyAnalyze(externalModules map[string][]string, local []PathLicense) (all AllModuleDependency) {
 	mainModule := ""
-	for _, tmpMap := range local {
-		for licensePath := range tmpMap {
-			var tmpArr []string
-			var tmpArr2 []string
-			modulePath := filepath.Dir(licensePath)
-			if !strings.Contains(modulePath, "/") && len(local) > 1 {
-				mainModule = modulePath
+	for _, nowPathLicense := range local {
+		logrus.Debug("path: ", nowPathLicense.Path, " license: ", nowPathLicense.License)
+
+		modulePath := filepath.Dir(nowPathLicense.Path)
+		if !strings.Contains(modulePath, "/") && len(local) > 1 {
+			mainModule = modulePath
+			continue
+		}
+		// 扫描此模块的依赖
+		var importArr []string
+		javaScan(filepath.Join(ProjectTmp, modulePath), &importArr)
+		logrus.Debug("import arr: ", importArr)
+		for i := range importArr {
+			importArr[i] = removeModuleSuffix(importArr[i])
+		}
+		importArr = removeRepeatElement(importArr)
+		logrus.Debug("import arr: ", importArr)
+		// 此模块非java依赖对应的外部依赖
+		var md ModuleDependency
+		md.Module = modulePath
+		for _, module := range importArr {
+			if strings.HasPrefix(module, "java.") {
 				continue
 			}
-			javaScan(filepath.Join(ProjectTmp, modulePath), &tmpArr)
-			//fmt.Println(tmpArr, "------------")
-			for _, s := range tmpArr {
-				tmpArr2 = append(tmpArr2, removeModuleSuffix(s))
-			}
-			for _, module := range removeRepeatElement(tmpArr2) {
-				if !strings.HasPrefix(module, "java.") {
-					for externalPath, external := range externalModules {
-						for _, v := range external {
-							if moduleEqual(3, strings.ReplaceAll(v, "/", "."), module) {
-								dependency[modulePath] = append(dependency[modulePath], externalPath)
-							}
-						}
+			for externalPath, external := range externalModules {
+				for _, v := range external {
+					if moduleEqualInDepthLevel(3, strings.ReplaceAll(v, "/", "."), module) {
+						md.Dependency = append(md.Dependency, externalPath)
+						break // 无需重复
 					}
 				}
 			}
 		}
+		md.Dependency = removeRepeatElement(md.Dependency)
+		all.Modules = append(all.Modules, md)
 	}
 
 	if mainModule != "" {
 		var mainModuleArr []string
-		for _, v := range dependency {
-			mainModuleArr = append(mainModuleArr, v...)
+		for _, md := range all.Modules {
+			mainModuleArr = append(mainModuleArr, md.Dependency...)
 		}
 		mainModuleArr = removeRepeatElement(mainModuleArr)
-		dependency[mainModule] = mainModuleArr
+		all.MainModule = ModuleDependency{Module: mainModule, Dependency: mainModuleArr}
 	}
-
-	// finally, remove repeat module
-	for k, v := range dependency {
-		dependency[k] = removeRepeatElement(v)
-	}
-	return dependency
+	return
 }
 
 /**
@@ -291,109 +305,117 @@ scan all java file to find import modules
 @param: result -> an array to store result
 */
 func javaScan(filePath string, result *[]string) {
-	if info, err := os.Stat(filePath); err == nil {
-		if !info.IsDir() {
-			if strings.HasSuffix(strings.ToLower(info.Name()), ".java") {
-				//fmt.Println(info.Name(), "-----------")
-				file, err := os.Open(filepath.Join(filePath))
-				if err != nil {
-					log.Fatal(err)
-				}
-				defer func(file *os.File) {
-					err := file.Close()
-					if err != nil {
-						fmt.Println(err.Error())
-					}
-				}(file)
+	info, err := os.Stat(filePath)
+	if err != nil {
+		panic(err)
+	}
+	// 对于目录
+	if info.IsDir() {
+		f, _ := os.Open(filePath)
+		defer func() {
+			if err := f.Close(); err != nil {
+				panic(err)
+			}
+		}()
+		names, _ := f.Readdirnames(0)
+		for _, name := range names {
+			newPath := filepath.Join(filePath, name)
+			javaScan(newPath, result)
+		}
+		return
+	}
+	// 对于java文件
+	if strings.HasSuffix(strings.ToLower(info.Name()), ".java") {
+		//fmt.Println(info.Name(), "-----------")
+		file, err := os.Open(filepath.Join(filePath))
+		if err != nil {
+			panic(err)
+		}
+		defer func() {
+			if err := file.Close(); err != nil {
+				panic(err)
+			}
+		}()
 
-				scanner := bufio.NewScanner(file)
-				for scanner.Scan() {
-					line := strings.Trim(scanner.Text(), " ")
-					if !strings.HasPrefix(line, "import ") {
-						continue
-					}
-					for {
-						if !strings.HasPrefix(line, "import ") {
-							break
-						}
-						*result = append(*result, strings.Trim(line[6:], " "))
-						scanner.Scan()
-						line = strings.Trim(scanner.Text(), " ")
-					}
-				}
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			line := strings.Trim(scanner.Text(), " ")
+			if !strings.HasPrefix(line, "import ") {
+				continue
 			}
-			return
-		} else {
-			f, _ := os.Open(filePath)
-			defer f.Close()
-			names, _ := f.Readdirnames(0)
-			for _, name := range names {
-				newPath := filepath.Join(filePath, name)
-				javaScan(newPath, result)
-			}
+			*result = append(*result, strings.Trim(line[6:], " "))
 		}
 	}
+	return
 }
 
 /**
 find all external modules from array of module path
-
+return package name in jar
 */
-func findAllExternalModule(tmp []map[string]interface{}) map[string][]string {
+func findAllExternalModule(external []PathLicense) map[string][]string {
 	result := make(map[string][]string)
 	arr := make([]string, 0)
-	for _, tmpMap := range tmp {
-		for key := range tmpMap {
-			filePath := filepath.Join(DIR, ProjectTmp, key)
-			if !strings.HasSuffix(key, ".jar") {
-				filePath = filepath.Dir(filePath)
+	for _, now := range external {
+		jarPath := now.Path
+		filePath := filepath.Join(DIR, ProjectTmp, jarPath)
+		/* 应该不存在此情况
+		if !strings.HasSuffix(jarPath, ".jar") {
+			filePath = filepath.Dir(filePath)
+		}*/
+		files, err := os.ReadDir(filePath)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		for _, file := range files {
+			if !file.IsDir() || strings.EqualFold(file.Name(), "META-INF") {
+				continue
 			}
-			files, err := os.ReadDir(filePath)
+			fileFullPath := filepath.Join(filePath, file.Name())
+			f, err := os.ReadDir(fileFullPath)
 			if err != nil {
 				log.Fatalln(err)
 			}
-			for _, file := range files {
-				if !file.IsDir() || strings.EqualFold(file.Name(), "META-INF") {
-					continue
+			for {
+				if !(len(f) == 1 && f[0].IsDir()) {
+					// 不只有一个文件 或 只有一个非目录文件（即搜到了包中）
+					break
 				}
-				tmpDir := filepath.Join(filePath, file.Name())
-				f, err := os.ReadDir(tmpDir)
+				fileFullPath = filepath.Join(fileFullPath, f[0].Name())
+				f, err = os.ReadDir(fileFullPath)
 				if err != nil {
 					log.Fatalln(err)
 				}
-				for {
-					if len(f) != 1 || (len(f) == 1 && !f[0].IsDir()) {
-						splitToken := ".jar\\"
-						if runtime.GOOS == "linux" {
-							splitToken = ".jar/"
-						}
-						result[key] = append(arr, strings.Split(tmpDir, splitToken)[1])
-						break
-					}
-					tmpDir = filepath.Join(tmpDir, f[0].Name())
-					f, err = os.ReadDir(tmpDir)
-					if err != nil {
-						log.Fatalln(err)
-					}
-				}
 			}
+			splitToken := ".jar\\"
+			if runtime.GOOS == "linux" {
+				splitToken = ".jar/"
+			}
+			result[jarPath] = append(arr, strings.Split(fileFullPath, splitToken)[1])
 		}
 	}
 	return result
 }
 
-func findAllLocalModule(tmp []map[string]interface{}) map[string]interface{} {
-	result := make(map[string]interface{})
-	for _, temMap := range tmp {
-		for key, value := range temMap {
-			module := filepath.Base(filepath.Dir(key))
+type ModuleLicense struct {
+	Module  string
+	License string
+}
+
+func findAllLocalModule(local []PathLicense) (result []ModuleLicense) {
+	for _, nowPathLicense := range local {
+		path := nowPathLicense.Path
+		license := nowPathLicense.License
+		module := filepath.Base(filepath.Dir(path))
+		/*
+			应该不存在此情况
 			if strings.EqualFold(module, ".") {
-				module = filepath.Dir(key)
+				module = filepath.Dir(path)
 			}
-			result[module] = value
-		}
+		*/
+		result = append(result, ModuleLicense{Module: module, License: license})
 	}
-	return result
+	return
 }
 
 /**
@@ -415,10 +437,10 @@ func removeRepeatElement(list []string) []string {
 }
 
 /**
-judge s1, s2 module equal or not
+judge s1, s2 module equal or not (only in first "depth" level)
 @param: depth -> the max depth
 */
-func moduleEqual(depth int, s1, s2 string) bool {
+func moduleEqualInDepthLevel(depth int, s1, s2 string) bool {
 	t1 := strings.Split(s1, ".")
 	t2 := strings.Split(s2, ".")
 	for i := 0; i < depth; i++ {
@@ -432,6 +454,7 @@ func moduleEqual(depth int, s1, s2 string) bool {
 	return true
 }
 
+// removeModuleSuffix if only one level, do nothing
 func removeModuleSuffix(module string) string {
 	re := regexp.MustCompile("\\..[^.]+;")
 	suffix := re.FindStringIndex(module)
