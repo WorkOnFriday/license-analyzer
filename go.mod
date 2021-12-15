b93c22e05b6e41cdba54509526bb3c2aa29f53b5
于斯同 <yusitong1999@foxmail.com>
2021-12-15 11:27:18 +0800

    测试: 原本scanner.go的main移动到测试文件

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/sirupsen/logrus v1.8.1
+	github.com/smartystreets/goconvey v1.7.2 // indirect
 	github.com/ugorji/go v1.2.6 // indirect
 	golang.org/x/crypto v0.0.0-20211117183948-ae814b36b871 // indirect
 	golang.org/x/sys v0.0.0-20211117180635-dee7805ff2e1 // indirect
---
+++ scanner/scanner_test.go
@@ -0,0 +1,61 @@
+package scanner
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/smartystreets/goconvey/convey"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+func TestCase(t *testing.T) {
+	t.Run("test", func(t *testing.T) {
+		convey.Convey("Test1", t, func() {
+			var param = "D:\\gopath\\src\\license-analyzer\\testProject\\ScannerTest4.zip"
+
+			suffix := strings.ToUpper(filepath.Ext(param))
+
+			if suffix == ".ZIP" || suffix == ".JAR" { //if file type is .zip, make tmp dir
+				if _, err := os.ReadDir(ProjectTmp); err != nil {
+					if err := os.MkdirAll(ProjectTmp, os.ModePerm); err != nil {
+						log.Fatalln(err)
+					}
+					defer os.RemoveAll(ProjectTmp)
+				}
+
+				external := make([]PathLicense, 0)
+				local := make([]PathLicense, 0)
+
+				zipDecompress(param, ProjectTmp)
+
+				//result = shallowScan(ProjectTmp)
+				deepScan(ProjectTmp, "", true, &local, &external)
+				defer os.RemoveAll(DIR)
+
+				t := map[string]interface{}{"external": external, "local": local}
+
+				marshal, err := json.Marshal(t)
+				if err != nil {
+					log.Fatalln(err)
+				}
+				fmt.Printf("Scan result: %v\n", string(marshal))
+				fmt.Println("all external modules: ", findAllExternalModule(external))
+				fmt.Println("all local modules: ", findAllLocalModule(local))
+				dependency, _ := json.Marshal(dependencyAnalyze(findAllExternalModule(external), local))
+				fmt.Println("dependency result: ", string(dependency))
+
+				m, d := PomScan("./pom.xml")
+				fmt.Println(m, "++++++++++", d)
+
+			} else if suffix == "" || suffix == ".TXT" || suffix == ".LICENSE" {
+				fmt.Println(param, ScanFile(param))
+			} else {
+				fmt.Println("Not support file")
+				os.Exit(0)
+			}
+		})
+	})
+}
